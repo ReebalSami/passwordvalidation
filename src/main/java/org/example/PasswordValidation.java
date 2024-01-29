cc9fbee76b701bc48e496df892bc75fd873a6719
Reebal Sami <reebal.sami@gmail.com>
2024-01-29 14:41:07 +0100

    method lenght Null added

--- src/main/java/org/example/PasswordValidation.java
+++ src/main/java/org/example/PasswordValidation.java
@@ -5,6 +5,9 @@ public class PasswordValidation {
     }
 
     public static boolean isPasswordLongEnough(String password){
+        if (password == null) {
+            return false;
+        }
         return password.length() >= 8;
     }
 
