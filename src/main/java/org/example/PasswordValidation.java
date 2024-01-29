41d80f379b5158a1063c947104ba0fa0422c2631
Reebal Sami <reebal.sami@gmail.com>
2024-01-29 15:57:17 +0100

    added bonus task 1 special Char test and method

--- src/main/java/org/example/PasswordValidation.java
+++ src/main/java/org/example/PasswordValidation.java
@@ -1,4 +1,10 @@
 package org.example;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PasswordValidation {
     public static void main(String[] args) {
         String password = "12f!ffffffT3";
@@ -8,17 +14,20 @@ public class PasswordValidation {
         System.out.println(isNotCommonPassword(password));
         System.out.println(containsSpecialCharacter(password));
 
+        String generatedPassword = generateSecurePassword();
+        System.out.println("Generated Password: " + generatedPassword);
+
 
     }
 
-    public static boolean isPasswordLongEnough(String password){
+    public static boolean isPasswordLongEnough(String password) {
         if (password == null) {
             return false;
         }
         return password.length() >= 8;
     }
 
-    public static boolean doesPasswordContainDigits(String password){
+    public static boolean doesPasswordContainDigits(String password) {
         for (char c : password.toCharArray()) {
             if (Character.isDigit(c)) {
                 return true;
@@ -28,30 +37,30 @@ public class PasswordValidation {
     }
 
 
-    public static boolean isUppercaseAndLowercase(String password){
+    public static boolean isUppercaseAndLowercase(String password) {
         boolean noUppercaseFound = true;
         boolean noLowercaseFound = true;
         int i = 0;
 
-        if(password == null) return false;
-        if(password.length()<2) return false;
+        if (password == null) return false;
+        if (password.length() < 2) return false;
 
-        while((noLowercaseFound||noUppercaseFound)&& i < password.length()){
-            if(Character.isLowerCase(password.charAt(i))) noLowercaseFound = false;
-            if(Character.isUpperCase(password.charAt(i))) noUppercaseFound = false;
+        while ((noLowercaseFound || noUppercaseFound) && i < password.length()) {
+            if (Character.isLowerCase(password.charAt(i))) noLowercaseFound = false;
+            if (Character.isUpperCase(password.charAt(i))) noUppercaseFound = false;
             i++;
         }
 
         return !(noLowercaseFound || noUppercaseFound);
     }
 
-    public static boolean isNotCommonPassword(String password){
-    String [] commonPasswords={"Password1","Aa345678","123456789"};
-    for (String commonPassword: commonPasswords){
-        if (password.equals(commonPassword)){
-            return false;
+    public static boolean isNotCommonPassword(String password) {
+        String[] commonPasswords = {"Password1", "Aa345678", "123456789"};
+        for (String commonPassword : commonPasswords) {
+            if (password.equals(commonPassword)) {
+                return false;
+            }
         }
-    }
         return true;
     }
 
@@ -64,6 +73,38 @@ public class PasswordValidation {
         return false;
     }
 
+    public static String generateSecurePassword() {
+        String uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        String digits = "0123456789";
+        String specialCharacters = "!@#$%^&*()-_+=<>?";
 
+        String allCharacters = uppercaseLetters + lowercaseLetters + digits + specialCharacters;
 
+        SecureRandom random = new SecureRandom();
+        List<Character> passwordCharacters = new ArrayList<>();
+
+        // Ensure the password has at least one character from each character set
+        passwordCharacters.add(uppercaseLetters.charAt(random.nextInt(uppercaseLetters.length())));
+        passwordCharacters.add(lowercaseLetters.charAt(random.nextInt(lowercaseLetters.length())));
+        passwordCharacters.add(digits.charAt(random.nextInt(digits.length())));
+        passwordCharacters.add(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
+
+        // Generate the rest of the password randomly
+        for (int i = 4; i < 8; i++) {
+            passwordCharacters.add(allCharacters.charAt(random.nextInt(allCharacters.length())));
+        }
+
+        // Shuffle the characters to make the order totally random
+        Collections.shuffle(passwordCharacters);
+
+        // Convert the List<Character> to a String
+        StringBuilder password = new StringBuilder();
+        for (char c : passwordCharacters) {
+            password.append(c);
+        }
+
+        return password.toString();
+
+    }
 }
